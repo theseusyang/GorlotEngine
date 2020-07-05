329d8460cbb63a27a3a3368122feaf0b3da21939
sallie <saraaahinreallife@gmail.com>
2020-07-05 21:57:02 +0000

    Derp

--- Editor/src/Editor.js
+++ Editor/src/Editor.js
@@ -64,8 +64,8 @@ Editor.MODE_ROTATE = 3;
 
 // Editor version
 Editor.NAME = "Gorlot"
-Editor.VERSION = "V0.0.2.1 Alpha"
-Editor.TIMESTAMP = "Sat 05 Jul 2020 20:44:24"
+Editor.VERSION = "V0.0.0.1-a"
+Editor.TIMESTAMP = "Sat 05 Jul 2020 21:56:50"
 
 // This is a variable for handling objects with a non-unique name
 Editor.nameId = 1
