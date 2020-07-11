a6214445217ef10e47a8a1af7eed59dd1f6a2f7e
sallie <saraaahinreallife@gmail.com>
2020-07-11 17:33:37 +0000

    Resize tool not affects physics shapes

--- Editor/src/Editor.js
+++ Editor/src/Editor.js
@@ -94,7 +94,7 @@ Editor.NAME = "Gorlot"
 Editor.VERSION = "V0.0.0.1-b dev"
 
 // TIMESTAMP is equals to Date()
-Editor.TIMESTAMP = "Sat Jul 11 2020 16:32:40 GMT+0000 (UTC)"
+Editor.TIMESTAMP = "Sat Jul 11 2020 17:34:22 GMT+0000 (UTC)"
 
 // This is a variable for handling objects with a non-unique name
 Editor.nameId = 1
--- Editor/src/core/objects/Text3D.js
+++ Editor/src/core/objects/Text3D.js
@@ -46,7 +46,9 @@ class Text3D extends THREE.Mesh {
 	// Dispose text
 	dispose() {
 		// Dispose material and geometry
-		this.material.dispose()
+		if (this.material.dispose !== undefined) {
+			this.material.dispose()
+		}
 		this.geometry.dispose()
 
 		// Dispose children
