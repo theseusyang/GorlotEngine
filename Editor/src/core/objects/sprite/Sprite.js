7818494d9046b12bf57f8a23a82f7b7a47320449
sallie <saraaahinreallife@gmail.com>
2020-07-30 18:28:06 +0000

    Updated opentype

--- Editor/src/core/objects/sprite/Sprite.js
+++ Editor/src/core/objects/sprite/Sprite.js
@@ -1,6 +1,5 @@
 "use strict";
 
-//Sprite constructor
 function Sprite(material)
 {
 	THREE.Sprite.call(this, material);
@@ -15,19 +14,15 @@ function Sprite(material)
 	this.defaultComponents.push(new ObjectComponent())
 }
 
-// Super prototype
 Sprite.prototype = Object.create(THREE.Sprite.prototype);
 
-// Dispose sprite
 Sprite.prototype.dispose = function()
 {
-	// Dispose material
 	if(this.material.dispose !== undefined)
 	{
 		this.material.dispose();
 	}
 
-	// Dispose children
 	for(var i = 0; i < this.children.length; i++)
 	{
 		this.children[i].dispose();
--- Editor/src/core/resources/Font.js
+++ Editor/src/core/resources/Font.js
@@ -23,7 +23,7 @@ function Font(url) {
 				this.data = JSON.parse(FileSystem.readFile(url))
 				this.name = this.data.original_font_information.fullName
 				this.format = "json"
-			} else if (this.encoding === "ttf") {
+			} else if (this.encoding === "ttf" || this.encoding === "otf" || this.encoding === "ttc" || this.encoding === "otc") {
 				this.data = new TTFLoader().parse(FileSystem.readFileArrayBuffer(url))
 				this.name = this.data.original_font_information.fullName
 				this.format = "json"
