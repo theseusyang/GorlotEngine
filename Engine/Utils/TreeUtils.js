231e65d32492aa5182bab03c82e68a2779412743
sallie <saraaahinreallife@gmail.com>
2020-08-01 21:55:05 +0000

    Updated tree utils

--- Engine/Utils/TreeUtils.js
+++ Engine/Utils/TreeUtils.js
@@ -64,38 +64,95 @@ Tree.prototype.print = function(level) {
 	}
 }
 
+function TreeUtils() {}
+
 // Tree changes mode
-Tree.DIFF_ADD = 0
-Tree.DIFF_REM = 1
-Tree.DIFF_MOVE = 2
+TreeUtils.DIFF_ADDED = 0
+TreeUtils.DIFF_REMOVED = 1
+TreeUtils.DIFF_MOVED = 2
 
 // Compare two trees and return list of changes
-Tree.compare = function(a, b, diffs, missing) {
-	if (diffs === undefined) {
+TreeUtils.compare = function(a, b, diffs, path_a, path_b) {
+	// Differences array
+	if(diffs === undefined) {
 		diffs = []
 	}
 
-	if (missing === undefined) {
-		missing = []
+	// Path to this tree point in positions
+	if(path_a === undefined) {
+		path_a = []
 	}
 
-	var length = (a.children.length < b.children.length) ? a.children.length : b.children.length
+	if (path_b === undefined) {
+		path_b = []
+	}
 
-	for(var i = 0, j = 0; i < length; i++, j++) {
-		if (a.children[i + 1].uuid === b.children[j].uuid) {
-			for(var k = 0; k < missing.length; k++) {
-				if (missing[k] === b.children[j].uuid) {
-					diffs.push("Moved (" + b.children[j].uuid + ")")
-				}
+	var i = 0, j = 0
+	while(i < a.children.length && j < b.children.length) {
+		if(a.children[i].uuid !== b.children[j].uuid) {
+			// Element missing (moved of deleted)
+			if(a.children[i + 1].uuid === b.children[j].uuid) {
+				var from = path_a.slice(0)
+				from.push(i)
 
-				if (k === missing.length) {
-					diffs.push("Removed (" + b.children[j].uuid + ")")
-				}
-			} else {
-				diffs.push("Added (" + b.children[j].uuid + ")")
+				diffs.push({status: TreeUtils.DIFF_REMOVED, uuid: a.children[i].uuid, from: from, to: null})
+				i++
 			}
+			// Added element
+			else if(a.children[i].uuid === b.children[j + 1].uuid) {
+				var to = path_b.slice(0)
+				to.push(j)
 
-			i++
+				diffs.push({status: TreeUtils.DIFF_ADDED, uuid: b.children[j].uuid, from: null, to: to})
+				j++
+			}
+		}
+		else {
+			var from = path_a.slice(0)
+			from.push(i)
+			var to = path_b.slice(0)
+			to.push(j)
+
+			TreeUtils.compare(a.children[i], b.children[j], diffs, from to)
+		}
+
+		i++
+		j++
+	}
+
+	// Remaining elements missing in a
+	while(i < a.children.length) {
+		var from = path_a.slice(0)
+		from.push(i)
+
+		diffs.push({status: TreeUtils.DIFF_REMOVED, uuid: a.children[i].uuid, from: from, to: null})
+		i++
+	}
+
+	// Extra elements added in b
+	while(j < b.children.length) {
+		var to = path_b.slice(0)
+		to.push(j)
+
+		diffs.push({status: TreeUtils.DIFF_ADDED, uuid: b.children[j].uuid, from: null, to: to})
+		j++
+	}
+
+	// Check if some elements have removed and added status at same time
+	for(var i = 0; i < diffs.length; i++) {
+		for(var j = 0; j < diffs.length; j++) {
+			if(diffs[i].uuid === diffs[j].uuid) {
+				if(diffs[i].status === TreeUtils.DIFF_REMOVED && diffs[j].status === TreeUtils.DIFF_ADDED) {
+					diffs[i].status = TreeUtils.DIFF_MOVED
+					diffs[i].to = diffs[j].to
+					diffs.splice(j, 1)
+				} else if(diffs[j].status === TreeUtils.DIFF_REMOVED && diffs[i].status === TreeUtils.DIFF_ADDED) {
+					diffs[i].status = TreeUtils.DIFF_MOVED
+					diffs[i].from = diffs[j].to
+					diffs.splice(j, 1)
+				}
+
+			}
 		}
 	}
 
